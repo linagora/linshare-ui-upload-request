c5383322d66a16b73443b8a5c13bde5c63ac7010
Camille Tjhoa <ctjhoa@linagora.com>
2014-07-08 14:55:58 +0200

    remove test logo

--- app/js/config.js
+++ app/js/config.js
@@ -1,5 +1,5 @@
 angular.module('app').constant('lsAppConfig', {
   backendURL: 'linshare',
-  customLogoURL: 'http://uploadrequest.local/img/logo_linshare.png', // eg: 'http://my.app/logo.png'
+  customLogoURL: undefined, // eg: 'http://my.app/logo.png'
   debug: true
 });

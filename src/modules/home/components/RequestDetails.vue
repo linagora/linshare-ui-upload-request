faa9fab7258ae300a4361abd8416d28863769fc3
Bao Khanh LE <bkle@linagora.com>
2020-10-01 15:12:01 +0700

    Issue #7: Add recipient list to a upload request

--- src/modules/home/components/RequestDetails.vue
+++ src/modules/home/components/RequestDetails.vue
@@ -33,6 +33,46 @@
                 {{ data.body }}
               </div>
             </div>
+            <div class="request-details-recipients">
+              <v-expansion-panels>
+                <v-expansion-panel>
+                  <v-expansion-panel-header class="mb-0 recipient-list-title">
+                    Recipient list
+                    <template v-slot:actions>
+                      <span class="recipient-toggle-icon">View</span>
+                      <v-icon color="#05B1FF">
+                        mdi-chevron-down
+                      </v-icon>
+                    </template>
+                  </v-expansion-panel-header>
+                  <v-expansion-panel-content>
+                    <v-list class="pa-0">
+                      <v-list-item
+                        v-for="recipient in data.recipients"
+                        :key="recipient.mail"
+                        class="ml-0 pl-0"
+                      >
+                        <v-list-item-avatar class="mr-2">
+                          <v-avatar
+                            size="36"
+                            :color="getColorByEmail(recipient.mail)"
+                          >
+                            <span class="white--text headline">
+                              {{ recipient.mail[0].toUpperCase() }}
+                            </span>
+                          </v-avatar>
+                        </v-list-item-avatar>
+                        <v-list-item-content>
+                          <v-list-item-title>
+                            {{ recipient.mail }}
+                          </v-list-item-title>
+                        </v-list-item-content>
+                      </v-list-item>
+                    </v-list>
+                  </v-expansion-panel-content>
+                </v-expansion-panel>
+              </v-expansion-panels>
+            </div>
             <div class="request-details-metadata">
               <div class="metadata-title">
                 Metadata
@@ -197,6 +237,33 @@ export default {
           .message-content {
             font-size: 15px;
             color: #5E5E5E;
+          }
+        }
+        .request-details-recipients {
+          padding-bottom: 10px;
+          border-bottom: 1px solid #E2E2E2;
+          .recipient-list-title {
+            line-height: 16px;
+            font-weight: 500;
+            font-size: 17px;
+            color: #5E5E5E;
+            margin-bottom: 10px;
+          }
+          .recipient-toggle-icon {
+            color: #05B1FF;
+            font-size: 12px;
+            margin-top: 4px;
+            font-weight: 400;
+          }
+          .v-expansion-panel::before {
+            box-shadow: none;
+          }
+          .v-expansion-panel-header {
+            padding: 0;
+            border: none;
+          }
+          .v-expansion-panel-content ::v-deep .v-expansion-panel-content__wrap {
+            padding: 0;
           }
         }
         .request-details-metadata {

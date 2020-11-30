c76288cb8c2963ad4788aed1bb3a434a65c072ed
Bao Ngoc PHAM <bnpham@linagora.com>
2020-11-30 15:49:13 +0700

    Issue #28: Add tests for new validations

--- tests/unit/common/validation.spec.js
+++ tests/unit/common/validation.spec.js
@@ -67,5 +67,76 @@ describe('validation.js', () => {
 
       expect(validateUpload(mockInput.files, mockInput.uploadRequest)).toMatch('MESSAGE.ERROR_NOT_ENOUGH_SPACE');
     });
+
+    it('should not return an error message if there is no limit for maxFileCount and other criterias are met', () => {
+      const mockInput = {
+        files: [{
+          name: 'file1',
+          size: 50
+        }, {
+          name: 'file2',
+          size: 120
+        }, {
+          name: 'file3',
+          size: 150
+        }],
+        uploadRequest: {
+          usedSpace: 0,
+          maxFileCount: -1,
+          maxFileSize: 500,
+          maxDepositSize: 10000,
+          currentFiles: []
+        },
+      };
+
+      expect(validateUpload(mockInput.files, mockInput.uploadRequest)).toBeUndefined;
+    });
+
+    it('should not return an error message if there is no limit for max size of a file and other criterias are met', () => {
+      const mockInput = {
+        files: [{
+          name: 'file1',
+          size: 3500
+        }, {
+          name: 'file2',
+          size: 3000
+        }, {
+          name: 'file3',
+          size: 3000
+        }],
+        uploadRequest: {
+          usedSpace: 0,
+          maxFileCount: 3,
+          maxDepositSize: 10000,
+          currentFiles: []
+        },
+      };
+
+      expect(validateUpload(mockInput.files, mockInput.uploadRequest)).toBeUndefined;
+    });
+
+    it('should not return an error message if there is no limit for max deposit and other criterias are met', () => {
+      const mockInput = {
+        files: [{
+          name: 'file1',
+          size: 3500
+        }, {
+          name: 'file2',
+          size: 3000
+        }, {
+          name: 'file3',
+          size: 3000
+        }],
+        uploadRequest: {
+          usedSpace: 0,
+          maxFileCount: 3,
+          maxFileSize: 5000,
+          maxDepositSize: 0,
+          currentFiles: []
+        },
+      };
+
+      expect(validateUpload(mockInput.files, mockInput.uploadRequest)).toBeUndefined;
+    });
   });
 });

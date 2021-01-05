258536d6e51f3d56cadb40f92c789353cdc98a69
Bao Khanh LE <bkle@linagora.com>
2021-01-05 15:18:20 +0700

    Issue #45: Fix unit tests for Vuex refactoring

--- tests/unit/modules/components/EntryList.spec.js
+++ tests/unit/modules/components/EntryList.spec.js
@@ -1,4 +1,5 @@
 import Vuetify from 'vuetify';
+import Vuex from 'vuex';
 import { mount, createLocalVue, config } from '@vue/test-utils';
 import EntryList from '@/modules/home/components/EntryList.vue';
 
@@ -7,49 +8,63 @@ config.mocks['$t'] = message => message;
 describe('Entrylist.vue', () => {
   const localVue = createLocalVue();
 
-  let vuetify;
+  localVue.use(Vuex);
+
+  let vuetify, store, getters;
 
   beforeEach(() => {
     vuetify = new Vuetify();
   });
 
   it('should show entry uploader mail on the table if upload request is collective', () => {
+    getters = {
+      uploadRequest: () => ({
+        collective: true,
+        closed: false
+      }),
+      entries: () => [{
+        name: 'linshare-vo-dich.png',
+        recipient: {
+          mail: 'bnpham@linagora.com'
+        }
+      }]
+    };
+
+    store = new Vuex.Store({
+      getters
+    });
+
     const wrapper = mount(EntryList, {
       localVue,
       vuetify,
-      propsData: {
-        data: {
-          collective: true,
-          closed: false
-        },
-        entries: [{
-          name: 'linshare-vo-dich.png',
-          recipient: {
-            mail: 'bnpham@linagora.com'
-          }
-        }]
-      }
+      store
     });
 
     expect(wrapper.find('.v-list-item__subtitle').text()).toMatch('bnpham@linagora.com');
   });
 
   it('should not show entry uploader mail on the table if upload request is not collective', () => {
+    getters = {
+      uploadRequest: () => ({
+        collective: false,
+        closed: false
+      }),
+      entries: () => [{
+        name: 'linshare-vo-dich.png',
+        recipient: {
+          mail: 'bnpham@linagora.com'
+        }
+      }]
+    };
+
+    store = new Vuex.Store({
+      getters
+    });
+
     const wrapper = mount(EntryList, {
       localVue,
       vuetify,
-      propsData: {
-        data: {
-          collective: false,
-          closed: false
-        },
-        entries: [{
-          name: 'linshare-vo-dich.png',
-          recipient: {
-            mail: 'bnpham@linagora.com'
-          }
-        }]
-      }
+      store
     });
 
     expect(wrapper.find('.v-list-item__subtitle').exists()).toBe(false);
